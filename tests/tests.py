4edf66288aeacaaa86330669d57ea3a81732f484
kabarton64 <22kabarton@gmail.com>
2023-10-05 14:32:10 -0500

    added test cases. 97% total

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pmgr.project import Project
+from pmgr.project import Project, TaskException
 
 @pytest.fixture(scope="function")
 def testproj():
@@ -11,4 +11,18 @@ def testproj():
 def test_add(testproj):
     testproj.add_task('dosomething')
     assert 'dosomething' in testproj.get_tasks()
+
+def test_fail_add(testproj):
+    testproj.add_task('dosomething')
+    with pytest.raises(TaskException):
+        testproj.add_task('dosomething')
+
+def test_remove(testproj):
+    testproj.add_task('dosomething')
+    testproj.remove_task('dosomething')
+    assert 'dosomething' not in testproj.get_tasks()
+
+def test_fail_remove(testproj):
+    with pytest.raises(TaskException):
+        testproj.remove_task('dosomething')
 
